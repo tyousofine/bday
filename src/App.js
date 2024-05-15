b7c5e81a29ce49f4a78c5a9d0015db3dee4303ff
tyousofine <tyousofine@gmail.com>
2024-05-14 20:26:15 -0400

    innitial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+      <h1>Happy Birthday Will!</h1>
+      <video className='vid'
+        src="vid.mp4"
+        loop
+        autoPlay
+        muted
+        controls
+      >test</video>
+    </div >
   );
 }
 
